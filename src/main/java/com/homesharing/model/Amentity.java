7ea04faa254b56bbe47d950d5d9747068e228f2a
Havey1053 <thanglthe186082@fpt.edu.vn>
2024-09-26 21:35:59 +0700

    add home and home's related classes

---
+++ src/main/java/com/homesharing/model/Amentity.java
@@ -0,0 +1,40 @@
+package com.homesharing.model;
+
+public class Amentity {
+    private int id;
+    private String name;
+    private String icon;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
