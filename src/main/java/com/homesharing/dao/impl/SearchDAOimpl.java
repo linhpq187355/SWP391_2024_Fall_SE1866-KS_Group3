267f17a8be8e67f98f2f22625ffe578eaf200793
datnguvl04 <125049024+datnguvl04@users.noreply.github.com>
2024-09-28 19:56:49 +0700

    search , home detail , terms , about us

--- src/main/java/com/homesharing/dao/impl/SearchDAOimpl.java
+++ src/main/java/com/homesharing/dao/impl/SearchDAOimpl.java
@@ -3,10 +3,9 @@ package com.homesharing.dao.impl;
 import com.homesharing.conf.DBContext;
 import com.homesharing.dao.SearchDAO;
 import com.homesharing.model.Home;
-import com.homesharing.model.Price; // Import the Price model
+import com.homesharing.model.Price;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,9 +17,9 @@ public class SearchDAOimpl implements SearchDAO {
 
     @Override
     public List<Home> searchHomes(String name, String location, Integer minPrice, Integer maxPrice) {
-        String sql = "SELECT h.*, p.id AS price_id, p.price AS price_value " +
+        String sql = "SELECT h.*, p.id AS priceId, p.price " +
                 "FROM Homes h " +
-                "JOIN Prices p ON h.id = p.Homesid " +
+                "JOIN Prices p ON h.id = p.homesId " +
                 "JOIN Wards w ON h.wardId = w.id " +
                 "JOIN Districts d ON w.Districtsid = d.id " +
                 "JOIN Provinces pr ON d.provincesId = pr.id " +
@@ -34,17 +33,17 @@ public class SearchDAOimpl implements SearchDAO {
         try (Connection connection = DBContext.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            // Set the parameters
+            // Set the parameters for the prepared statement
             preparedStatement.setString(1, "%" + name + "%");
             preparedStatement.setString(2, name);
             preparedStatement.setString(3, name);
             preparedStatement.setString(4, "%" + location + "%");
             preparedStatement.setString(5, location);
             preparedStatement.setString(6, location);
-            preparedStatement.setInt(7, minPrice != null ? minPrice : 0); // Set default minPrice if null
-            preparedStatement.setInt(8, maxPrice != null ? maxPrice : Integer.MAX_VALUE); // Set default maxPrice if null
-            preparedStatement.setInt(9, minPrice);
-            preparedStatement.setInt(10, maxPrice);
+            preparedStatement.setInt(7, minPrice != null ? minPrice : 0);
+            preparedStatement.setInt(8, maxPrice != null ? maxPrice : Integer.MAX_VALUE);
+            preparedStatement.setObject(9, minPrice); // This allows for null
+            preparedStatement.setObject(10, maxPrice); // This allows for null
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
@@ -61,21 +60,22 @@ public class SearchDAOimpl implements SearchDAO {
                     home.setNumOfBedroom(resultSet.getInt("numOfBedroom"));
                     home.setNumOfBath(resultSet.getInt("numOfBath"));
                     home.setCreatedDate(resultSet.getTimestamp("createdDate").toLocalDateTime());
-                    home.setModifiedDate(resultSet.getTimestamp("modifiedDate") != null ? resultSet.getTimestamp("modifiedDate").toLocalDateTime() : null);
+                    home.setModifiedDate(resultSet.getTimestamp("modifiedDate") != null ?
+                            resultSet.getTimestamp("modifiedDate").toLocalDateTime() : null);
                     home.setHomeDescription(resultSet.getString("homeDescription"));
                     home.setTenantDescription(resultSet.getString("tenantDescription"));
                     home.setWardId(resultSet.getInt("wardId"));
-                    home.setHomeType(resultSet.getInt("homeTypeId"));
+                    home.setHomeTypeId(resultSet.getInt("homeTypeId"));
                     home.setCreatedBy(resultSet.getInt("createdBy"));
 
-                    // Create a Price object
+                    // Create and set the Price object
                     Price price = new Price();
-                    price.setId(resultSet.getInt("price_id")); // Get the price ID
-                    price.setPrice(resultSet.getBigDecimal("price_value")); // Get the price value
-                    price.setHomeId(home.getId()); // Set the corresponding home ID
+                    price.setId(resultSet.getInt("priceId"));
+                    price.setPrice(resultSet.getInt("price"));
 
-                    // You can now store the price object or manage it according to your needs
-                    // For example, if you want to keep it in the home, you may need to add a list of prices in Home model
+                    // If you want to associate price with home, you can do it like this
+                    home.setPriceId(price.getId());
+                    // Optionally, store price as a field in the Home model if needed
 
                     homes.add(home);
                 }
