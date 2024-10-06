918e8cf46f587aa0f10da4db17d178104094b5cd
manhNC2k4 <manhnc2k4@gmail.com>
2024-10-06 11:15:48 +0700

    login = google update

--- src/main/java/com/homesharing/conf/GGConfig.java
+++ src/main/java/com/homesharing/conf/GGConfig.java
@@ -45,13 +45,4 @@ public class GGConfig {
     /** The URL to get user information using the access token. */
     public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
 
-    public static void main(String[] args) {
-        // Kiểm tra và in ra giá trị của các biến môi trường
-        System.out.println("Google Client ID: " + GOOGLE_CLIENT_ID);
-        System.out.println("Google Client Secret: " + GOOGLE_CLIENT_SECRET);
-        System.out.println("Google Redirect URI: " + GOOGLE_REDIRECT_URI);
-        System.out.println("Google Grant Type: " + GOOGLE_GRANT_TYPE);
-        System.out.println("Google Link Get Token: " + GOOGLE_LINK_GET_TOKEN);
-        System.out.println("Google Link Get User Info: " + GOOGLE_LINK_GET_USER_INFO);
-    }
 }
