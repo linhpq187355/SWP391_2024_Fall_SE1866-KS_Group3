ba9e909f8dacd42a931e35cb3692960a48ecd752
Havey1053 <thanglthe186082@fpt.edu.vn>
2024-10-31 07:58:01 +0700

    add missing func in the previous push

--- src/main/java/com/homesharing/dao/impl/ProvinceDAOImpl.java
+++ src/main/java/com/homesharing/dao/impl/ProvinceDAOImpl.java
@@ -1,9 +1,11 @@
 package com.homesharing.dao.impl;
 
 import com.homesharing.conf.DBContext;
+import com.homesharing.dao.DistrictDAOImpl;
 import com.homesharing.dao.ProvinceDAO;
 import com.homesharing.exception.GeneralException;
 import com.homesharing.model.Province;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,8 +13,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ProvinceDAOImpl implements ProvinceDAO {
+    public final String DB_RESOURCE = "Error closing database resources:";
+    Logger logger = Logger.getLogger(DistrictDAOImpl.class.getName());
+
     @Override
     public List<Province> getAllProvinces() {
         List<Province> provinces = new ArrayList<>();
@@ -55,5 +61,46 @@ public class ProvinceDAOImpl implements ProvinceDAO {
             }
         }
         return provinces;
+    }
+
+    @Override
+    public Province getProvinceById(int id) {
+        String sql = "SELECT * FROM [dbo].[Provinces] WHERE id=?";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = DBContext.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                Province province = new Province();
+                province.setId(resultSet.getInt("id"));
+                province.setName(resultSet.getString("name"));
+                province.setStatus(resultSet.getString("status"));
+                return province;
+            }
+        } catch (SQLException | IOException | ClassNotFoundException e) {
+            // Re-throw as runtime exception to be handled by the service layer
+            throw new IllegalArgumentException("Error saving to the database: " + e.getMessage(), e);
+        } finally {
+            // Closing resources in reverse order of opening
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                logger.severe(DB_RESOURCE + e.getMessage());
+            }
+        }
+        return null;
     }
 }
